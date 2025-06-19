64b5004161e908f52e3a380de8ceb5ce2f2014ac
Sai-developer-6699 <shukruneai@gmail.com>
2025-06-19 15:52:23 +0530

    Update addinfo and dashboard pages

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'recruitment',
         'USER': 'root',
-        'PASSWORD': '12345',
+        'PASSWORD': 'Mysql@2004',
         'HOST': 'localhost',
         'PORT': '3306',
     }
